df551a4c7cb36290125b265d339f16dbe8eb7df8
DongHyeokJeong <ddkkd1@naver.com>
2015-02-12 08:07:44 +0900

    signin

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,25 +1,51 @@
     $(document).ready(function(){
 
-       // $.ajax({
-       //    url: '/'
-       //    ,type:"get"
-       //    ,success: function(data){
-       //      console.log("connect");
-       //    }
-       //    ,error: function(req,state,err){
-       //      console.log("err");
-       //    }
-       //  });
+      $('#signin_submit').click(function(){
+            var userID = $('#signin_email').val();
+            var userPwd = $('#signin_password').val();
+            var userName = $('#signin_name').val();
+            var userPwd_confirm = $('#password_confirm').val();
 
 
-        $('#submit').click(function(){
+
+            if ( ((userID == "") || (userPwd == "")) || (userName == "") ) {
+                alert("Insert All Information!!!");
+            } else {
+                if(userPwd == userPwd_confirm) {
+                var json = {};
+                json["email"]  =  userID;
+                json["password"] =  userPwd;
+                json["userName"] =  userName;
+
+
+                $.ajax({
+                    type:'post',
+                    url:'/signin',
+                    data:json,
+                    success:function(result) {
+                        if ( result.status == "FAIL" ) {
+                            alert('This id already used');
+                        } else if ( result.status == "SUCCESS" ) {
+                            alert('sign in success');
+                        }
+                    }
+                });
+              }
+              else {
+                alert('confirm password not match');
+              }
+            }
+        });
+
+
+        $('#login_submit').click(function(){
+
             var userID = $('#login_email').val();
             var userPwd = $('#login_password').val();
 
             if ( (userID == "") || (userPwd == "") ) {
                 alert("Insert Your Information!");
             } else {
-
                 var json = {};
                 json["email"]  =  userID;
                 json["password"] =  userPwd;
@@ -29,13 +55,10 @@
                     url:'/login',
                     data:json,
                     success:function(result) {
-                      console.log(result.status);
-                        if ( result.status == "FAIL" ) {
+                        if (result.status == "FAIL") {
                             alert('nononononono ㅗ');
-                        } else if ( result.status == "SUCCESS" ) {
-                          console.log('gooddddddddddd!');
-                          //window.location.href="/views/login success.html";
-                          //$(location).attr('href', 'views/login success.html'); //move page
+                        } else {
+                          $(location).attr('href', '/test'); //move page
                         }
                     }
                 });
@@ -44,7 +67,7 @@
     });
 
 
-function login_open(idFirDiv,idSecDiv){
+function login_open(idFirDiv,idSecDiv){                                                             //jquery랑 javascript랑 같이 쓰면 안되요!!
 	var objDiv = document.getElementById(idFirDiv);
 	var objDiv2 = document.getElementById(idSecDiv);
 
