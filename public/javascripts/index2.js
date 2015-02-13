fdd6c06d7234bc2ab8e0aa1806ec4ccc148dd344
epfptk423 <epfptk423@naver.com>
2015-02-13 17:06:37 +0900

    modified

--- public/javascripts/index2.js
+++ public/javascripts/index2.js
@@ -11,7 +11,7 @@ function signin_open(idFirDiv,idSecDiv){
 	var objDiv = document.getElementById(idFirDiv);
 	var objDiv2 = document.getElementById(idSecDiv);
 
-    if(objDiv.style.display=="none")
+    if(objDiv.style.display=="none" || objDiv.style.display=="")
     	{ objDiv.style.display = "block"; objDiv2.style.display = "none"; }
     else{ objDiv.style.display = "none"; objDiv2.style.display = "block"; }
 }
