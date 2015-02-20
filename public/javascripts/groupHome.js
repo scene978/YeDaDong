f21eae93026384bffe7c6d496e8a23956d794cf4
DongHyeokJeong <ddkkd1@naver.com>
2015-02-20 14:30:29 +0900

    groupHome!

--- public/javascripts/groupHome.js
+++ public/javascripts/groupHome.js
@@ -1,9 +1,44 @@
+$(document).ready(function() {
 
-$(function() {
+	$.ajax({
+		type : 'get',
+		url : '/mypage/helloMessage',
+		success : function(data) {
+			$('#btnHelloMessage').html(data.name);
+		}
+	});
+
+	$.ajax({
+		type : 'post',
+		url : '/mypage/scrollGroupList',
+		success : function(data) {
+			
+			var htmlString;
+			$.each(data, function(index,value){
+				htmlString += "<option value=\"" + data[index].groups+"\">"+data[index].groups+"</option>";
+			});
+			$('#groupList').html(htmlString);
+			
+		}
+	});
+
+	$("#btnLogout").click(function() {
+		console.log("Logout click");
+		$.ajax({
+			type : 'get',
+			url : '/mypage/logout',
+			success : function(data) {
+				console.log("Logout Success");
+				$(location).attr('href', '/');
+			}
+		});
+	});
+
 	$("#profile").click(function() {
 		layer_open('image_upload_popup', 'layer');
 		return false;
 	});
+
 });
 
 function layer_open(layer_id, higher_layer_class) {
--- routes/groupHome.js
+++ routes/groupHome.js
@@ -17,3 +17,38 @@ exports.index = function(req, res) {
 		res.redirect('./');
 	}
 };
+
+exports.logout = function(req, res) {
+	req.session.destroy(function() {
+		console.log("Route: Logout");
+	});
+
+	res.write("");
+	res.end();
+};
+
+exports.helloMessage = function(req, res) {  //다솜이 시키기
+	var id = req.session.user_id;
+	client.query('select member_name from member where id=?', [id], function(err, rows) {
+		if (err) {
+			console.log(err);
+		} else {
+			var name = {
+				'name' : rows[0].member_name
+			};
+			res.send(name);
+		}
+	});
+};
+
+exports.scrollGroupList = function(req, res){
+	var id = req.session.user_id;
+	
+	client.query('select group_name as groups from member_group natural join group_list where id=? and member_group_state=\'1\' and group_state=\'1\'', [id], function(err, rows) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.send(rows);
+		}
+	});
+};
