b9a0fe0eeaa8be0c5f4fbf5f20490f2c476037e2
scene978 <scene978@gmail.com>
2015-02-22 17:10:05 +0900

    main page

--- public/javascripts/groupHome.js
+++ public/javascripts/groupHome.js
@@ -34,4 +34,59 @@ $(document).ready(function() {
 		});
 	});
 
+	/*-----------------------------------------------------Send message popup script----------------------------------------------------------------*/
+
+$(".btnSendmessage").click(function() {
+		layer_open('popupSendmessage', 'layer');
+		return false;
+	});
+	
+$(".sendpersonMessagebox").click(function() {
+		layer_open('popupSendmessage', 'layer');
+		return false;
+	});
+		
+$(".textMessagebox").click(function() {
+		layer_open('popupRvdmessage', 'layer2');
+		return false;
+	});
+	
+function layer_open(layer_id, higher_layer_class) {
+
+	var popup_layer = $('#' + layer_id);
+	var higher_layer_class = $('.' + higher_layer_class);
+	var background = popup_layer.prev().hasClass('background');
+
+	if (background) {
+		higher_layer_class.fadeIn();
+		// 'background' 클래스가 존재하면 레이어가 나타나고 배경은 dimmed 된다.
+	} else {
+		popup_layer.fadeIn();
+	}
+
+	// 화면의 중앙에 레이어를 띄운다.
+	if (popup_layer.outerHeight() < $(document).height())
+		popup_layer.css('margin-top', '-' + popup_layer.outerHeight() / 2 + 'px');
+	else
+		popup_layer.css('top', '0px');
+	if (popup_layer.outerWidth() < $(document).width())
+		popup_layer.css('margin-left', '-' + popup_layer.outerWidth() / 2 + 'px');
+	else
+		popup_layer.css('left', '0px');
+
+	popup_layer.find('a.btnClose').click(function(e) {
+		if (background) {
+			higher_layer_class.fadeOut();
+			// 'background' 클래스가 존재하면 레이어를 사라지게
+			// 한다.
+		} else {
+			popup_layer.fadeOut();
+		}
+		e.preventDefault();
+
+		window.location.reload(true);
+		// close버튼을 누르면 페이지 초기화->나중에 통신할 때 서버로 값 넘겨주고 초기화할때 값 받아오게 하기
+	});
+}
+
 });
